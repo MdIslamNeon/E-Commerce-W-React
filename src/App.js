272ea98b73bffe23b04beb523808361dfcaf70b6
Md Islam <2003raisul@gmail.com>
2026-06-26 14:57:07 -0700

    books page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route } from "react-router-dom/cjs/react-router-dom.min"; 
 import Home from "./pages/Home";
 import Books from "./pages/Books";
+import BookInfo from "./pages/BookInfo";
+import { books } from "./data";
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
       <div className="App">
         <Nav />
         <Route path='/' exact component={Home} />
-        <Route path='/books'component={Books} />
+        <Route path='/books' exact render={() => <Books books={books} />} />
+        <Route path='/books/:id' render={() => <BookInfo books={books} />} />
         <Footer />
       </div>
     </Router>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Book from '../components/ui/Book'
 
-function Books() {
+function Books({books: initialBooks}) {
+
+  const [books, setBooks] = useState(initialBooks)
+
+  function filterBooks(filter) {
+    console.log(filter)
+    if (filter === 'LOW_TO_HIGH') {
+        setBooks(books.slice().sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)))
+    }
+    if (filter === 'HIGH_TO_LOW') {
+        setBooks(books.slice().sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)))
+    }
+    if (filter === 'RATING') {
+        setBooks(books.slice().sort((a, b) => (b.rating || b.rating) - (a.rating || a.rating)))
+    }
+  }
+
   return (
     <div id="book__body">
         <main id="books__main">
@@ -9,13 +26,17 @@ function Books() {
                     <div className="row">
                         <div className="books__header">
                             <h2 className='section__title books__header--title'>All Books</h2>
-                            <select id="filter">
-                                <option value="" selected disabled>Sort</option>
+                            <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterBooks(event.target.value)}>
+                                <option value="DEFAULT" disabled>Sort</option>
                                 <option value="LOW_TO_HIGH">Price, Low to High</option>
                                 <option value="HIGH_TO_LOW">Price, High to Low</option>
-                                <option value="RATING">Rating</option>
-                                
+                                <option value="RATING">Rating</option>           
                             </select>
+                        </div>
+                        <div className="books">
+                            {
+                                books.map(book => <Book book={book} key={book.id} />)
+                            }
                         </div>
                     </div>
                 </div>
